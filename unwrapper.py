1aa5c9743563db1503a7d84f8f6c940a87180ee4
ItsDixie <hipster.yt89@mail.ru>
2023-05-11 22:50:01 +0300

    Small update

--- unwrapper.py
+++ unwrapper.py
@@ -15,6 +15,7 @@ msgC = {}
 badC = {}
 
 #-------#
+pd.options.display.max_rows = 200
 
 with open('data.json', 'r') as save:
         rough = json.load(save) # массив -> массивы -> словари
@@ -33,71 +34,87 @@ with open('stats.txt', 'w') as f:
     sys.stdout = f
 
     #---------------users-----------
-    names = []
-    arr1 = np.empty((1,1))
-    arr2 = np.empty((1,1))
+    def unwrapU(msgU, badU):
+        names = []
+        arr1 = np.empty((1,1))
+        arr2 = np.empty((1,1))
 
-    for key in msgU.keys():
-        names.append(key)
-        arr1 = np.append(arr1, msgU[key])
-        try:
-            arr2 = np.append(arr2, badU[key])
-        except Exception:
-            arr2 = np.append(arr2, 0)
+        for key in msgU.keys():
+            names.append(key)
+            arr1 = np.append(arr1, msgU[key])
+            try:
+                arr2 = np.append(arr2, badU[key])
+            except Exception:
+                arr2 = np.append(arr2, 0)
 
-    arr1 = arr1.astype(float) # fixing
-    arr1 = arr1.astype(int)   #  bugs
-    arr1 = arr1[arr1 != 0] # удаяю нули
-    arr2 = arr2.astype(float) # fixing
-    arr2 = arr2.astype(int)   # fixing
-    arr2 = arr2[1:]           # удаяю нули
+        arr1 = arr1.astype(float) # fixing
+        arr1 = arr1.astype(int)   #  bugs
+        arr1 = arr1[arr1 != 0] # удаяю нули
+        arr2 = arr2.astype(float) # fixing
+        arr2 = arr2.astype(int)   # fixing
+        arr2 = arr2[1:]           # удаяю нули
 
-    dataU = {'id': names,
-            'words': np.round(arr1+arr2),
-            'amount of bad words': np.round(arr2),
-            '%% of bad words': np.round(arr2/(arr1+arr2)*100)}
+        dataU = {'id': names,
+                'words': np.round(arr1+arr2),
+                'amount of bad words': np.round(arr2),
+                '%% of bad words': np.round(arr2/(arr1+arr2)*100)}
 
-    # convert dictionary to dataframe
-    dfU = pd.DataFrame(dataU)
+        
+        dfU = pd.DataFrame(dataU)
 
-    # print dataframe
-    print("---------------users-----------")
-    print(dfU)
-    print("-------------------------------\n")
+        dfU = dfU.sort_values('words', ascending=False)
+        dfU.to_csv (r'USERSdataKAIJU.csv', index= False )
+        print("---------------users-----------")
+        print(dfU)
+        print('\n')
+        print(dfU.describe())
+        print("-------------------------------\n")
     #---------------users-----------
-
+    
     #---------------chats-----------
-    nick = []
-    arr3 = np.empty((1,1))
-    arr4 = np.empty((1,1))
+    def unwrapC(msgC, badC):
+        nick = []
+        arr3 = np.empty((1,1))
+        arr4 = np.empty((1,1))
 
-    for key in msgC.keys():
-        nick.append(f"<#{key}>")
-        arr3 = np.append(arr3, msgC[key])
-        try:
-            arr4 = np.append(arr4, badC[key])
-        except Exception:
-            arr4 = np.append(arr4, 0)
-    arr3 = arr3.astype(float) # fixing
-    arr3 = arr3.astype(int)   #  bugs
-    arr3 = arr3[arr3 != 0] # удаяю нули
-    arr4 = arr4.astype(float) # fixing
-    arr4 = arr4.astype(int)   # fixing
-    arr4 = arr4[1:]  # удаяю нули
+        for key in msgC.keys():
+            nick.append(f"<#{key}>")
+            arr3 = np.append(arr3, msgC[key])
+            try:
+                arr4 = np.append(arr4, badC[key])
+            except Exception:
+                arr4 = np.append(arr4, 0)
+        arr3 = arr3.astype(float) # fixing
+        arr3 = arr3.astype(int)   #  bugs
+        arr3 = arr3[arr3 != 0] # удаяю нули
+        arr4 = arr4.astype(float) # fixing
+        arr4 = arr4.astype(int)   # fixing
+        arr4 = arr4[1:]  # удаяю нули
 
-    dataC = {'id of chat': nick,
-            'words': np.round(arr3+arr4),
-            'amount of bad words': np.round(arr4),
-            '%% of bad words': np.round(arr4/(arr3+arr4)*100)}
+        dataC = {'id of chat': nick,
+                'words': np.round(arr3+arr4),
+                'amount of bad words': np.round(arr4),
+                '%% of bad words': np.round(arr4/(arr3+arr4)*100)}
 
-    dfC = pd.DataFrame(dataC)
+        dfC = pd.DataFrame(dataC)
 
-    print(f"Amount of msg: {np.sum(arr3)+np.sum(arr4)}\n")
-    print(f"Amount of msgs with bad words: {np.sum(arr4)}\n")
+        print(f"Amount of msg: {np.sum(arr3)+np.sum(arr4)}\n")
+        print(f"Amount of msgs with bad words: {np.sum(arr4)}\n")
 
-    print("---------------chats-----------")
-    print(dfC)
-    print("-------------------------------\n")
+        dfC = dfC.sort_values('words', ascending=False)
+        dfC.to_csv (r'CHATSdataKAIJU.csv', index= False )
+        print("---------------chats-----------")
+        print(dfC)
+        print('\n')
+        print(dfC.describe())
+        print("-------------------------------\n")
+    #---------------chats----------- 
+    unwrapU(msgU, badU)
+    unwrapC(msgC, badC)
+    
 
-    # Restore the original standard output
     sys.stdout = original_stdout
+
+
+    # (pd.concat([dfU, dfC], ignore_index=True) - объединить датафреймы)
+    # (pd.fillna(0) = заменить пустоты на 0)
